485ffd9208b3b484ad770e49798be5c1e1b01981
vamship <vamshi.ponnapalli@gmail.com>
2015-05-07 15:19:31 -0400

    Refactored test structure

--- test/server/routes/public-routes-spec.js
+++ test/server/routes/public-routes-spec.js
@@ -2,24 +2,32 @@
 'use strict';
 var _config = require('../../config');
 var _supertest = require('supertest');
+var _path = require('path');
 var expect = require('chai').expect;
 
-describe('/', function() {
+describe('[public routes]', function() {
+    var MOUNT_PATH = '/public/';
     var request = _supertest(_config.baseUrl);
 
-    it('should show the application status when invoked', function(done) {
-        request.get('/public/__status')
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function(err, res) {
-                expect(err).to.be.null;
+    function _getPath(path) {
+        return _path.join(MOUNT_PATH, path);
+    }
 
-                var payload = res.body;
-                expect(payload).to.be.defined;
-                expect(payload.app).to.equal('node-rest-template');
-                expect(payload.version).to.be.a('string');
-                expect(payload.timestamp).to.be.a('number');
-                done();
-            });
+    describe('GET /__status', function() {
+        it('should show the application status when invoked', function(done) {
+            request.get(_getPath('/__status'))
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(function(err, res) {
+                    expect(err).to.be.null;
+
+                    var payload = res.body;
+                    expect(payload).to.be.defined;
+                    expect(payload.app).to.equal('node-rest-template');
+                    expect(payload.version).to.be.a('string');
+                    expect(payload.timestamp).to.be.a('number');
+                    done();
+                });
+        });
     });
 });
